085988c5fcea6ce2c85d409a131281588960c9a8
koral <74123610+koralreeef@users.noreply.github.com>
2025-03-05 13:26:33 -0800

    clean debug mod without custom stats

---
+++ objects/player/stats_debug.gd
@@ -0,0 +1,71 @@
+extends Label
+
+# AUGHHH make a dict later
+var damage = 0
+var defense = 0
+var luck = 0
+var evasiveness = 0
+var speed = 0
+var squirt = 0
+var trap = 0
+var lure = 0
+var sound = 0
+var throw = 0
+var drop = 0
+var loadout = GagLoadout
+
+func _ready() -> void:
+	if SaveFileService.settings_file.show_timer:
+		show()
+
+func _process(delta: float) -> void:
+	var fps = str(Engine.get_frames_per_second())
+	if not Util.get_player():
+		print("couldn't find player")
+		return
+	else:
+		var player = Util.get_player()
+		battle_speed = SettingsFile.SpeedOptions[SaveFileService.settings_file.get('battle_speed_idx')]
+		if BattleService.ongoing_battle != null:
+			damage = BattleService.ongoing_battle.battle_stats[Util.get_player()].damage
+			defense = BattleService.ongoing_battle.battle_stats[Util.get_player()].defense
+			evasiveness = BattleService.ongoing_battle.battle_stats[Util.get_player()].evasiveness
+			luck = BattleService.ongoing_battle.battle_stats[Util.get_player()].luck
+		else:
+			damage = player.stats.damage
+			defense = player.stats.defense
+			evasiveness = player.stats.defense
+			luck = player.stats.luck
+		squirt = player.stats.gags_unlocked["Squirt"]
+		trap = player.stats.gags_unlocked["Trap"]
+		lure = player.stats.gags_unlocked["Lure"]
+		sound = player.stats.gags_unlocked["Sound"]
+		throw = player.stats.gags_unlocked["Throw"]
+		drop = player.stats.gags_unlocked["Drop"]
+		
+	text = "fps: %s 
+	battle speed: %.2fx 
+	damage: %.2f 
+	defense: %.2f
+	evasiveness: %.2f
+	luck: %.2f
+	speed: %.2f
+	squirt: lv.%.0f
+	trap: lv.%.0f
+	lure: lv.%.0f
+	sound: lv.%.0f
+	throw: lv.%.0f
+	drop: lv.%.0f" % [
+		fps,
+		battle_speed, 
+		damage,
+		defense,
+		evasiveness,
+		luck,
+		speed,
+		squirt,
+		trap,
+		lure,
+		sound,
+		throw,
+		drop]
