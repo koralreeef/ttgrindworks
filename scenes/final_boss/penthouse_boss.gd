f57fe6b3e5f82916e627292ea36e6182409e6002
koral <74123610+koralreeef@users.noreply.github.com>
2025-03-14 10:20:10 -0700

    whoops its level 25 not 20

--- scenes/final_boss/penthouse_boss.gd
+++ scenes/final_boss/penthouse_boss.gd
@@ -44,7 +44,7 @@ func _ready() -> void:
 	set_caged_toon_dna(get_caged_toon_dna())
 	AudioManager.set_music(MUSIC_TRACK)
 	# Pick the first boss
-	boss_cog.level = Util.floor_number * 4
+	boss_cog.level = Util.floor_number * 5
 	var boss_choices := possible_bosses.duplicate()
 	if DEBUG_FORCE_BOSS_ONE != null and OS.is_debug_build() and WANT_DEBUG_BOSSES:
 		boss_one_choice = DEBUG_FORCE_BOSS_ONE
@@ -54,7 +54,7 @@ func _ready() -> void:
 	boss_choices.erase(boss_one_choice)
 
 	# Pick the second boss
-	boss_cog_2.level = Util.floor_number * 4
+	boss_cog_2.level = Util.floor_number * 5
 	if DEBUG_FORCE_BOSS_TWO != null and OS.is_debug_build() and WANT_DEBUG_BOSSES:
 		boss_two_choice = DEBUG_FORCE_BOSS_TWO
 	else:
